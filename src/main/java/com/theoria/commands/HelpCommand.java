14581c9efff1d95340468dea847a0e2e51662587
James Keenan <monsterbrains@protonmail.com>
2026-03-12 00:53:48 -0400

    docs: include MIT LICENSE for open source usage

--- src/main/java/com/theoria/commands/HelpCommand.java
+++ src/main/java/com/theoria/commands/HelpCommand.java
@@ -13,15 +13,16 @@ public class HelpCommand implements Commands {
     @Override
     public void execute(String name, StackerLogic stacker) throws Exception {
 
-        System.out.println(CLIFontColors.CYAN + "================ PDFStacker CLI - All Merge & Sort Commands =================" + CLIFontColors.RESET);
+        System.out.println(CLIFontColors.CYAN + "================ PDFStacker CLI - All Merge & Sort Commands =================================================" + CLIFontColors.RESET);
         System.out.println(CLIFontColors.YELLOW + "Available Commands:" + CLIFontColors.RESET);
-        System.out.println(CLIFontColors.GREEN + "  add    <path>        " + CLIFontColors.RESET + "                 - Add a PDF file to the stack");
-        System.out.println(CLIFontColors.GREEN + "  list              " + CLIFontColors.RESET + "                    - Show current PDFs in the stack");
-        System.out.println(CLIFontColors.GREEN + "  remove <number>   " + CLIFontColors.RESET + "                    - Remove PDF at given position");
-        System.out.println(CLIFontColors.GREEN + "  move   <currentIndex> -> <newIndex> " + CLIFontColors.RESET + "  - Move PDF up in the stack");
-        System.out.println(CLIFontColors.GREEN + "  merge  <output.pdf>" + CLIFontColors.RESET + "                   - Merge all PDFs into one file");
-        System.out.println(CLIFontColors.GREEN + "  exit              " + CLIFontColors.RESET + "                    - Exit the application");
-        System.out.println(CLIFontColors.CYAN + "=============================================================================" + CLIFontColors.RESET);
+        System.out.println(CLIFontColors.GREEN + "  add           <path>        " + CLIFontColors.RESET + "                   - Add a PDF file to the stack");
+        System.out.println(CLIFontColors.GREEN + "  list              " + CLIFontColors.RESET + "                             - Show current PDFs in the stack");
+        System.out.println(CLIFontColors.GREEN + "  startOver             " + CLIFontColors.RESET + "                         - Remove everything and start over.");
+        System.out.println(CLIFontColors.GREEN + "  remove        <indexNumber>   " + CLIFontColors.RESET + "                 - Remove PDF at given position");
+        System.out.println(CLIFontColors.GREEN + "  move          <currentIndex> -> <newIndex> " + CLIFontColors.RESET + "    - Move PDF up in the stack");
+        System.out.println(CLIFontColors.GREEN + "  merge         <output.pdf>" + CLIFontColors.RESET + "                     - Merge all PDFs into one file");
+        System.out.println(CLIFontColors.GREEN + "  exit              " + CLIFontColors.RESET + "                             - Exit the application");
+        System.out.println(CLIFontColors.CYAN + "=============================================================================================================" + CLIFontColors.RESET);
 
     }
 
